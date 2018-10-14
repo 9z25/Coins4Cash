4a6ae15159d08866b6b3b6506bc44cb4b9db0f24
9z25 <rsenecaljr@gmail.com>
2018-10-14 08:11:52 -0700

    Update coinAjax.js

--- public/coinAjax.js
+++ public/coinAjax.js
@@ -1,12 +1,54 @@
+$.ajaxTransport("+binary", function(options, originalOptions, jqXHR){
+    // check for conditions and support for blob / arraybuffer response type
+    if (window.FormData && ((options.dataType && (options.dataType == 'binary')) || (options.data && ((window.ArrayBuffer && options.data instanceof ArrayBuffer) || (window.Blob && options.data instanceof Blob)))))
+    {
+        return {
+            // create new XMLHttpRequest
+            send: function(headers, callback){
+        // setup all variables
+                var xhr = new XMLHttpRequest(),
+        url = options.url,
+        type = options.type,
+        async = options.async || true,
+        // blob or arraybuffer. Default is blob
+        dataType = options.responseType || "blob",
+        data = options.data || null,
+        username = options.username || null,
+        password = options.password || null;
+                    
+                xhr.addEventListener('load', function(){
+            var data = {};
+            data[options.dataType] = xhr.response;
+            // make callback and send data
+            callback(xhr.status, xhr.statusText, data, xhr.getAllResponseHeaders());
+                });
+ 
+                xhr.open(type, url, async, username, password);
+                
+        // setup custom headers
+        for (var i in headers ) {
+            xhr.setRequestHeader(i, headers[i] );
+        }
+                
+                xhr.responseType = dataType;
+                xhr.send(data);
+            },
+            abort: function(){
+                jqXHR.abort();
+            }
+        };
+    }
+});
+
 var coinAjax = {
 //get list of connected peers
 getConnectedPeers: function(){
-   var getPeers = $.ajax({
+    var getPeers = $.ajax({
+    async: false,
     url: 'http://localhost:4002/ob/peers',
     headers: {
         'Authorization':'Basic YzRjdGVzdGVyOlN0YXJ0QDEyMzY2Ng==',
     },
-    async: false,
     method: 'GET',
     dataType: 'json',
     success: function(data){
@@ -14,19 +56,18 @@ getConnectedPeers: function(){
     },
 
     error: function(a,b,c) {
-      alert('process error' + JSON.stringify(a) +" :: " + b + " :: " + c)
-    }
-})
+        alert(a + b + c)
+    },
+  });
 
-   return JSON.stringify(getPeers.responseText)
-},
 
 //map each segment of array as parameter for getStoreData() func
 //peerList.map(getStoreData)
-
+return JSON.parse(getPeers.responseText)
+},
 
 //get shop listings thru ajax call **peer ID hash required**
-getPeerListing: function(pID){
+getPeerListings: function(pID){
     var openStore = $.ajax({
     async: false,
     url: 'http://localhost:4002/ob/listings/' + pID,
@@ -39,40 +80,73 @@ getPeerListing: function(pID){
     },
 
     error: function(a,b,c) {
-      alert('process error' + JSON.stringify(a) +" :: " + b + " :: " + c)
+        alert(a + b + c)
     },
   });
 return JSON.parse(openStore.responseText)
-}
+},
 
 //load Avatar for store button **image hash required**
-getImg: function (imgHash){
-var getImage = $.ajax({
+getImg: function (store){
+
+    var getImage = $.ajax({
     async: false,
-    url: 'http://localhost:4002/ob/images/' + imgHash,
+    url: 'http://localhost:4002/ob/images/' + store.avatarHashes.small,
     headers: {
+        'Content-Type':'image/png','X-Requested-With':'XMLHttpRequest',
         'Authorization':'Basic YzRjdGVzdGVyOlN0YXJ0QDEyMzY2Ng==',
     },
+    dataType : "binary",
+    processData : false,
     method: 'GET',
-    dataType: 'img',
+    //mimeType: 'text/plain; charset=x-user-defined',
     success: function(data){
     },
 
     error: function(a,b,c) {
-      alert('process error' + JSON.stringify(a) +" :: " + b + " :: " + c)
     },
+  }).done(function( data, textStatus, jqXHR ) {
+    var urlCreator = window.URL || window.webkitURL;
+   var imageUrl = urlCreator.createObjectURL(data);
+
+   var button = document.createElement("BUTTON");
+button.setAttribute("href","#");
+button.setAttribute("id",store.name)
+button.setAttribute("class", "w3-button w3-black")
+
+var h = document.createElement("p")
+var t = document.createTextNode(store.name);
+var u = document.createTextNode(store.shortDescription);
+
+document.getElementById("store-list").appendChild(h);
+h.appendChild(button);
+button.appendChild(t);
+   button.appendChild(document.createElement("br"));
+   button.appendChild(u);
+
+var imm = document.createElement("IMG")
+imm.setAttribute("src",imageUrl)
+imm.setAttribute("width", "304");
+imm.setAttribute("height", "228");
+
+button.appendChild(imm)
+
+            document.getElementById(store.name).addEventListener('click', function() {
+                alert(store.name)
+                alert(store.shortDescription)
+                alert(store.currencies)
+                var listings = coinAjax.getPeerListings(store.peerID)
+                items(listings)
+                // i can be used anywhere in this scope
+            })
+
   });
 
-var content = Base64.btoa(getImage.responseText)
-
-return content
-
-}
+},
 
 //get data on store thru ajax call **peer ID has required**
 getStoreData: function(pID){
-
-var loadcoinAjax = $.ajax({
+var getProfile = $.ajax({
     async: false,
     url: 'http://localhost:4002/ob/profile/' + pID,
     headers: {
@@ -84,10 +158,11 @@ var loadcoinAjax = $.ajax({
     },
 
     error: function(a,b,c) {
-      alert('process error' + JSON.stringify(a) +" :: " + b + " :: " + c)
-    },
+        alert(a + " :: " + b + " :: " + c)
+    }
   })
 
-return JSON.parse(loadcoinAjax.responseText)
+return JSON.parse(getProfile.responseText)
 
+}
 }
